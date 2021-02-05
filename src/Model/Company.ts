8a55be70715d4717e101986a112489a7d2c09c38
Gleidson Leite <gledson.leytte@hotmail.com>
2021-02-05 09:21:11 -0300

    Model Company with schema

---
+++ src/Model/Company.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema, Document, Model, model } from 'mongoose';
+
+interface ICompany extends Document {
+  name: string;
+  personName: string;
+  email: string;
+  password: string;
+  bitrix_id: string;
+  cpf_cnpj: string;
+}
+
+const CompanySchema: Schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  personName: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  bitrix_id: {
+    type: Number,
+  },
+
+  cpf_cnpj: {
+    type: String,
+    required: true,
+  },
+});
+
+const Company: Model<ICompany> = model('Company', CompanySchema);
+
+export default Company;
