2b9dcc07935ce6d1a2035e82bd51460df00ceda4
Gleidson Leite <gledson.leytte@hotmail.com>
2021-03-11 11:25:31 -0300

    Mail with templates

--- src/Services/SendEmailService.ts
+++ src/Services/SendEmailService.ts
@@ -1,14 +1,25 @@
 import { SendMailOptions } from 'nodemailer';
 import transporter from '../config/email';
+import endpointsConfig from '../config/endpoints.config';
+
+interface Request extends SendMailOptions {
+  template?: string;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  context?: Object;
+}
 
 class SendEmailService {
-  public async execute(mailOptions: SendMailOptions): Promise<boolean> {
+  public async execute(mailOptions: Request): Promise<boolean> {
     return transporter
-      .sendMail(mailOptions)
+      .sendMail({
+        ...mailOptions,
+        from: `Plataforma LCC<${endpointsConfig.mailUserName}>`,
+      } as SendMailOptions)
       .then(() => {
         return true;
       })
-      .catch(() => {
+      .catch(info => {
+        console.log(info);
         return false;
       });
   }
