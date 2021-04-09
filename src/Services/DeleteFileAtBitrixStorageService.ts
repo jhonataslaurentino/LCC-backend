925fb92719d5cfde9972fe309149e8ee99e1183b
Gleidson Leite <gledson.leytte@hotmail.com>
2021-04-08 22:33:09 -0300

    ignoring delete file error

--- src/Services/DeleteFileAtBitrixStorageService.ts
+++ src/Services/DeleteFileAtBitrixStorageService.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from 'axios';
 import bitrixApi from '../api/bitrix';
 import bitrixApiMethods from '../api/Bitrix/bitrixMethods';
 
@@ -7,11 +8,21 @@ interface Request {
 
 class DeleteFileAtBitrixStorageService {
   public async execute({ fileId }: Request): Promise<void> {
-    await bitrixApi.post(bitrixApiMethods.DELETE_FILE, null, {
-      params: {
-        id: fileId,
-      },
-    });
+    try {
+      await bitrixApi.post(bitrixApiMethods.DELETE_FILE, null, {
+        params: {
+          id: fileId,
+        },
+      });
+    } catch (error) {
+      if (error.response) {
+        const err = error as AxiosError;
+        if (err.response.status === 400) {
+          // eslint-disable-next-line no-console
+          console.log('File does not exists');
+        }
+      }
+    }
   }
 }
 
