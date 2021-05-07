0b410cc877b8d4e3d205aad740b29d9c545da36f
Gleidson Leite <gledson.leytte@hotmail.com>
2021-05-06 20:03:44 -0400

    Remove permission from role

---
+++ src/Services/Roles/RemovePermissionFromRoleService.ts
@@ -0,0 +1,30 @@
+import RoleModel from '../../Entities/Role';
+import Role from '../../Schemas/Role';
+import VerifyIfHasPermissionService from './VerifyIfHasPermissionService';
+
+interface Request {
+  roleID: string;
+  permissionValue: number;
+}
+
+class RemovePermissionFromRoleService {
+  public async execute({ roleID, permissionValue }: Request): Promise<Role> {
+    const role = await RoleModel.findById(roleID);
+    if (!role) {
+      throw new Error('Role does not exists');
+    }
+    const verifyIfHasPermissionService = new VerifyIfHasPermissionService();
+    const hasPermission = verifyIfHasPermissionService.execute({
+      rolePermissionValue: role.permissions,
+      permissionValue,
+    });
+    if (!hasPermission) {
+      throw new Error('Role does not have this permission');
+    }
+    role.permissions -= permissionValue;
+    await role.save();
+    return role;
+  }
+}
+
+export default RemovePermissionFromRoleService;
