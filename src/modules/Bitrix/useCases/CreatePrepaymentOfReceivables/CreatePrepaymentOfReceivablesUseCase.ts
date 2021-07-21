aba6187eaeef577b1d447a333799df598b0070bf
Jhonatas Laurentino <costajhonatas9@gmail.com>
2021-07-21 18:31:19 -0300

    Update CreatePrepaymentOfReceivablesUseCase.ts
    Update e finalização do CreatePrepaymentOfReceivablesUseCase at

--- src/modules/Bitrix/useCases/CreatePrepaymentOfReceivables/CreatePrepaymentOfReceivablesUseCase.ts
+++ src/modules/Bitrix/useCases/CreatePrepaymentOfReceivables/CreatePrepaymentOfReceivablesUseCase.ts
@@ -1,6 +1,8 @@
+import AppError from '../../../../errors/AppError';
 import { ICompanyRepository } from "../../../company/repositories/ICompanyRepository";
 import { IBitrixContactRepository } from "../../repositories/IBitrixContactRepository";
 import { IBitrixDealRepository, ICreatePrepaymentOfReceivablesDTO } from "../../repositories/IBitrixDealRepository";
+import { BitrixDeal } from '../../../Bitrix/schemas/BitrixDeal';
 
 interface IRequest
   extends Omit<ICreatePrepaymentOfReceivablesDTO, 'contactID'>{
@@ -20,8 +22,50 @@ class CreatePartnerPrepaymentOfReceivablesUseCase {
   ){}
 
   async execute({
-
-  })
+    annualInvoice,
+    associateCPF,
+    associateRG,
+    companyID,
+    email,
+    name,
+    opportunityValue,
+    personType,
+    phone,
+    proofOfAddress,
+    proofOfBusinessAddress,
+    socialContract,
+    birthday,
+    cpf,
+  }: IRequest): Promise<BitrixDeal> {
+  const company = await this.companiesRepository.findByID(companyID);
+  if (!company) {
+    throw new AppError('Company does not exists', 404);
+  }
+  const contact = await this.bitrixContactsRepository.create({
+    birthday,
+    companyID: company.bitrix_id,
+    email,
+    name,
+    cpf,
+    personType,
+    phone,
+  });
+  const prepaymentOfReceivablesDeal = await this.bitrixDealsRepository.CreatePrepaymentOfReceivablesDeal(
+    {
+      annualInvoice,
+      associateCPF,
+      associateRG,
+      companyID: String(company.bitrix_id),
+      contactID: contact.ID,
+      opportunityValue,
+      proofOfAddress,
+      proofOfBusinessAddress,
+      socialContract,
+    },
+    );
+    return prepaymentOfReceivablesDeal;
+  }
 }
+
   export {CreatePartnerPrepaymentOfReceivablesUseCase}
 
