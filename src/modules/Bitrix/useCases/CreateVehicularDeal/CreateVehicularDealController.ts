dd796013aff6ca281f810f11898ae7cb18ec0684
Jhonatas Laurentino <costajhonatas9@gmail.com>
2021-07-21 14:19:25 -0300

    Create CreateVehicularDealController.ts
    ADD CreateVehicularDealController at CreateVehicularDeal\UseCases

---
+++ src/modules/Bitrix/useCases/CreateVehicularDeal/CreateVehicularDealController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from 'express';
+import { CreateVehicularDealUseCase } from './CreateVehicularDealUseCase';
+
+interface IFiles {
+  [key: string]: Express.Multer.File[];
+}
+
+class CreateVehicularDealController{
+  constructor(private CreateVehicularDealUseCase: CreateVehicularDealUseCase) {}
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    const files = request.files as IFiles;
+    const {
+      birthday,
+      companyID,
+      email,
+      name,
+      cnpj,
+      cpf,
+      personType,
+      phone,
+    }= request.body;
+
+    const createdDeal = await this.CreateVehicularDealUseCase.execute({
+      companyID,
+      email,
+      name,
+      cpf,
+      cnpj,
+      phone,
+      clientSituation,
+      contactMonthlyIncome,
+      opportunityValue,
+      vehicleManufacturedDate,
+      vehicleModel,
+      vehicleName,
+      vehicleTargetValue,
+      vehicleValue,
+      vehicularCreditType,
+      address,
+      birthday,
+      personType,
+    });
+    return response.json(createdDeal);
+  }
+}
+
+export { CreateVehicularDealUseCase };
